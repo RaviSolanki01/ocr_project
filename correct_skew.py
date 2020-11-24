6b22959aa8227405aade6865a0fe1fb89a50adb1
Ravi Solanki <Ravisolanki5466@gmail.com>
2020-11-24 11:55:07 +0530

    skew correction issue resolved

--- correct_skew.py
+++ correct_skew.py
@@ -1,63 +1,37 @@
-# USAGE
-# python correct_skew.py --image images/neg_28.png
-
-# import the necessary packages
-import numpy as np
-import argparse
 import cv2
+import numpy as np
+from scipy.ndimage import interpolation as inter
 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True,
-	help="path to input image file")
-args = vars(ap.parse_args())
+def correct_skew(image, delta=1, limit=5):
+    def determine_score(arr, angle):
+        data = inter.rotate(arr, angle, reshape=False, order=0)
+        histogram = np.sum(data, axis=1)
+        score = np.sum((histogram[1:] - histogram[:-1]) ** 2)
+        return histogram, score
 
-# load the image from disk
-image = cv2.imread(args["image"])
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1] 
 
-# convert the image to grayscale and flip the foreground
-# and background to ensure foreground is now "white" and
-# the background is "black"
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-gray = cv2.bitwise_not(gray)
+    scores = []
+    angles = np.arange(-limit, limit + delta, delta)
+    for angle in angles:
+        histogram, score = determine_score(thresh, angle)
+        scores.append(score)
 
-# threshold the image, setting all foreground pixels to
-# 255 and all background pixels to 0
-thresh = cv2.threshold(gray, 0, 255,
-	cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+    best_angle = angles[scores.index(max(scores))]
 
-# grab the (x, y) coordinates of all pixel values that
-# are greater than zero, then use these coordinates to
-# compute a rotated bounding box that contains all
-# coordinates
-coords = np.column_stack(np.where(thresh > 0))
-angle = cv2.minAreaRect(coords)[-1]
+    (h, w) = image.shape[:2]
+    center = (w // 2, h // 2)
+    M = cv2.getRotationMatrix2D(center, best_angle, 1.0)
+    rotated = cv2.warpAffine(image, M, (w, h), flags=cv2.INTER_CUBIC, \
+              borderMode=cv2.BORDER_REPLICATE)
 
-# the `cv2.minAreaRect` function returns values in the
-# range [-90, 0); as the rectangle rotates clockwise the
-# returned angle trends to 0 -- in this special case we
-# need to add 90 degrees to the angle
-if angle < -45:
-	angle = -(90 + angle)
+    return best_angle, rotated
 
-# otherwise, just take the inverse of the angle to make
-# it positive
-else:
-	angle = -angle
-
-# rotate the image to deskew it
-(h, w) = image.shape[:2]
-center = (w // 2, h // 2)
-M = cv2.getRotationMatrix2D(center, angle, 1.0)
-rotated = cv2.warpAffine(image, M, (w, h),
-	flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
-
-# draw the correction angle on the image so we can validate it
-cv2.putText(rotated, "Angle: {:.2f} degrees".format(angle),
-	(10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-
-# show the output image
-print("[INFO] angle: {:.3f}".format(angle))
-cv2.imshow("Input", image)
-cv2.imshow("Rotated", rotated)
-cv2.waitKey(0)
+if __name__ == '__main__':
+    image = cv2.imread('doc.png')
+    angle, rotated = correct_skew(image)
+    print(angle)
+    cv2.imshow('rotated', rotated)
+    cv2.imwrite('rotated.png', rotated)
+    cv2.waitKey(0)
